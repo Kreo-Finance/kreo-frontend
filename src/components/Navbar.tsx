5339d69caba344a202e32676e6e16dd4edd7ea3a
CodeBlocker52 <codeblocker736@gmail.com>
2026-03-27 17:23:15 +0530

    updated the navbar ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -181,7 +181,7 @@ const Navbar = () => {
                 <a
                   key={link.label}
                   href={link.href}
-                  className="font-body text-sm font-medium text-muted-foreground transition-all hover:text-foreground hover:translate-y-[-1px]"
+                  className="font-body text-base font-bold text-muted-foreground transition-all hover:text-foreground hover:translate-y-[-1px]"
                 >
                   {link.label}
                 </a>
@@ -189,7 +189,7 @@ const Navbar = () => {
                 <Link
                   key={link.label}
                   to={link.href}
-                  className={`font-body text-sm font-medium transition-all hover:text-foreground hover:translate-y-[-1px] ${location.pathname === link.href
+                  className={`font-body text-base font-bold transition-all hover:text-foreground hover:translate-y-[-1px] ${location.pathname === link.href
                     ? "text-foreground"
                     : "text-muted-foreground"
                     }`}
@@ -265,7 +265,7 @@ const Navbar = () => {
                     <a
                       key={link.label}
                       href={link.href}
-                      className="font-body text-sm text-muted-foreground hover:text-foreground"
+                      className="font-body text-base font-bold text-muted-foreground hover:text-foreground"
                       onClick={() => setMobileOpen(false)}
                     >
                       {link.label}
@@ -274,7 +274,7 @@ const Navbar = () => {
                     <Link
                       key={link.label}
                       to={link.href}
-                      className="font-body text-sm text-muted-foreground hover:text-foreground"
+                      className="font-body text-base font-bold text-muted-foreground hover:text-foreground"
                       onClick={() => setMobileOpen(false)}
                     >
                       {link.label}
